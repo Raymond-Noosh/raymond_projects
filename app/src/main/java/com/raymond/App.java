bf4c75e604574128ef9bde2ce80d21da1410b0cb
Your Name <you@example.com>
2016-03-23 00:14:52 -0700

    save

--- app/src/main/java/com/raymond/App.java
+++ app/src/main/java/com/raymond/App.java
@@ -1,6 +1,10 @@
 package com.raymond;
 
+import com.raymond.beans.Person;
+import com.raymond.config.AppConfig;
 import com.raymond.examples.Singleton;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Hello world!
@@ -11,6 +15,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Person person = ctx.getBean(Person.class);
+        person.setEmail("raym123@hotmail.com");
+        person.setName("Raymond");
+
+
         Singleton.getInstance();
     }
 }
--- core/src/main/java/com/raymond/examples/RaymondThreadLocal.java
+++ core/src/main/java/com/raymond/examples/RaymondThreadLocal.java
@@ -4,4 +4,5 @@ package com.raymond.examples;
  * Created by Raymond Kwong on 3/22/2016.
  */
 public class RaymondThreadLocal {
+
 }
