c785535a47aa3cef62cdeb354110307235ba0d86
Your Name <you@example.com>
2016-05-26 00:17:56 -0700

    save

---
+++ boot/src/main/java/com/raymond/map/RaymondMap.java
@@ -0,0 +1,7 @@
+package com.raymond.map;
+
+/**
+ * Created by Raymond Kwong on 5/26/2016.
+ */
+public class RaymondMap {
+}
---
+++ boot/src/main/java/com/raymond/stack/RaymondStack.java
@@ -0,0 +1,7 @@
+package com.raymond.stack;
+
+/**
+ * Created by Raymond Kwong on 5/26/2016.
+ */
+public class RaymondStack {
+}
