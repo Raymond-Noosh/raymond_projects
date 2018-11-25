8073d73487aa7fb87fa9f78cdea31a7c54d8be3d
Your Name <you@example.com>
2018-11-24 20:55:15 -0800

    save

---
+++ services/jwt-authorization-server/src/main/java/com/raymond/JWTAuthorizationApplication.java
@@ -0,0 +1,16 @@
+package com.raymond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * Created by Raymond Kwong on 11/24/2018.
+ */
+@SpringBootApplication
+public class JWTAuthorizationApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JWTAuthorizationApplication.class, args);
+    }
+}
