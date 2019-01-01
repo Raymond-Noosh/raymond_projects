5d58d99799170d43c9d39f1bb3f1aec9e2232459
Your Name <you@example.com>
2018-12-31 22:07:27 -0800

    save

--- services/resource/src/main/java/com/raymond/resource/controller/SampleResourceController.java
+++ services/resource/src/main/java/com/raymond/resource/controller/SampleResourceController.java
@@ -1,21 +1,39 @@
 package com.raymond.resource.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.raymond.resource.dto.UserCredentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Raymond Kwong on 12/31/2018.
  */
-@RestController()
+@RestController
 @RequestMapping("sample")
 public class SampleResourceController {
+
+    private final static Logger logger = LoggerFactory.getLogger(SampleResourceController.class);
 
     @GetMapping("/test")
     public ResponseEntity<String> testErrors() {
         ResponseEntity responseEntity = new ResponseEntity("test is ok", HttpStatus.OK);
+        return responseEntity;
+    }
+
+    @PostMapping("/create")
+    @ResponseBody
+    public ResponseEntity<String> create(@RequestBody UserCredentials userCredentials) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            logger.info(mapper.writeValueAsString(userCredentials));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        ResponseEntity responseEntity = new ResponseEntity("create is ok", HttpStatus.OK);
         return responseEntity;
     }
 }
