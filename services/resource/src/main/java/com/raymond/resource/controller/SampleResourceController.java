87f684f40065b95d44973b8f04ec36f0c57d4cde
Your Name <you@example.com>
2019-01-09 00:37:49 -0800

    save

--- services/resource/src/main/java/com/raymond/resource/controller/SampleResourceController.java
+++ services/resource/src/main/java/com/raymond/resource/controller/SampleResourceController.java
@@ -6,6 +6,10 @@ import com.raymond.resource.dto.UserCredentials;
 import com.raymond.resource.dto.UserDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,15 +17,25 @@ import org.springframework.web.bind.annotation.*;
 /**
  * Created by Raymond Kwong on 12/31/2018.
  */
+@RefreshScope
 @RestController
 @RequestMapping("sample")
 public class SampleResourceController {
+
+    @Autowired
+    private Environment environment;
 
     private final static Logger logger = LoggerFactory.getLogger(SampleResourceController.class);
 
     @GetMapping("/test")
     public ResponseEntity<String> testErrors() {
-        ResponseEntity responseEntity = new ResponseEntity("test is ok", HttpStatus.OK);
+        String[] activeProfiles = environment.getActiveProfiles();      // it will return String Array of all active profile.
+        String port = environment.getProperty("local.server.port");
+        StringBuffer buffer = new StringBuffer();
+        for(String profile:activeProfiles) {
+            buffer.append(profile);
+        }
+        ResponseEntity responseEntity = new ResponseEntity("test is ok, using port:"+port, HttpStatus.OK);
         return responseEntity;
     }
 
