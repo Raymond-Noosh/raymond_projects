e0f2694bd2de910ae0627481ec22a45dcb07efba
Your Name <you@example.com>
2019-01-12 12:29:40 -0800

    save

--- services/jwt-auth/start.bat
+++ services/jwt-auth/start.bat
@@ -1,3 +1,3 @@
-cd /d C:\raymond_projects\services\jwt-authorization-server
+cd /d C:\raymond_projects\services\jwt-auth
 set MAVEN_OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=n
 mvn spring-boot:run
--- services/jwt-auth/start2.bat
+++ services/jwt-auth/start2.bat
@@ -1,2 +1,2 @@
-cd /d C:\raymond_projects\services\jwt-authorization-server
+cd /d C:\raymond_projects\services\jwt-auth
 mvn spring-boot:run -Dspring-boot.run.jvmArguments="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=n"
