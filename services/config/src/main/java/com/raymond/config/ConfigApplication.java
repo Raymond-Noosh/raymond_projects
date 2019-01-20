11526b3731127b9c5d10dd0cce3a081bd0955786
Your Name <you@example.com>
2019-01-19 18:53:06 -0800

    save

--- services/config/src/main/java/com/raymond/config/ConfigApplication.java
+++ services/config/src/main/java/com/raymond/config/ConfigApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class ConfigApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
 	}
