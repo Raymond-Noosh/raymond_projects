e0c3c68b417a16cef92d32b239859e7e16696673
Your Name <you@example.com>
2019-01-22 21:53:48 -0800

    save, rebuilds

---
+++ services/eureka/src/main/java/com/raymond/eureka/controller/RefreshScopeController.java
@@ -0,0 +1,21 @@
+package com.raymond.eureka.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Raymond Kwong on 1/22/2019.
+ */
+@RefreshScope
+@RestController
+public class RefreshScopeController {
+    @Value("${app.service-name}")
+    private String serviceName;
+
+    @GetMapping("/service")
+    public String getServiceName() {
+        return "service name: " + this.serviceName;
+    }
+}
