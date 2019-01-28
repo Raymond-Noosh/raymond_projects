ec8818f5fd2b9e0840982233548e30241e75c035
Your Name <you@example.com>
2019-01-27 21:59:28 -0800

    save

--- services/cloudgateway/src/main/java/com/raymond/gateway/CloudGatewayApplication.java
+++ services/cloudgateway/src/main/java/com/raymond/gateway/CloudGatewayApplication.java
@@ -2,6 +2,9 @@ package com.raymond.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by Raymond Kwong on 1/27/2019.
@@ -11,5 +14,15 @@ public class CloudGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/resource")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .build();
     }
 }
