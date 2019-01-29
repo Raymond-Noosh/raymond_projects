8e02d7efd422e8aefbc31890addb003764fbc5b3
Your Name <you@example.com>
2019-01-28 21:11:53 -0800

    save

--- services/resource/src/main/java/com/raymond/resource/config/RefreshScopeConfig.java
+++ services/resource/src/main/java/com/raymond/resource/config/RefreshScopeConfig.java
@@ -2,10 +2,12 @@ package com.raymond.resource.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Raymond Kwong on 1/28/2019.
  */
+@Component
 @ConfigurationProperties
 public class RefreshScopeConfig {
 
